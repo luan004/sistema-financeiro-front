c58679ff3dfa2ed9e04aafc2d91f680ebd5db2ca
Luan Gabriel <38366327+luan004@users.noreply.github.com>
2026-08-19 22:07:33 -0300

    feat: utiliza localização em português no momentjs

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import moment from 'moment'
-import 'moment/locale/pt-br'
 import App from './App.jsx'
+import moment from 'moment'
+import 'moment/dist/locale/pt-br';
+import './index.css'
 
 moment.locale('pt-br')
 
